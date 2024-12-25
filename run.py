0c60733d8c3733224c64f75b31cb3c11d4597634
salman644 <salmanquluzade20@gmail.com>
2024-12-26 01:09:37 +0400

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,8 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
 
