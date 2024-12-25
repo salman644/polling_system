6ca4af4cb034748ce8da675a8a7e35dac9254f11
salman644 <salmanquluzade20@gmail.com>
2024-12-26 00:07:26 +0400

    Update poll.py

--- models/poll.py
+++ models/poll.py
@@ -1,1 +1,14 @@
+class Poll:
+    def __init__(self, title, description):
+        self.id = None
+        self.title = title
+        self.description = description
+        self.options = []  # List of Option objects
+        self.votes = {}  # Dictionary to keep track of votes
 
+    def add_option(self, option):
+        self.options.append(option)
+        self.votes[option.id] = 0
+
+    def get_results(self):
+        return {option.text: self.votes[option.id] for option in self.options}
