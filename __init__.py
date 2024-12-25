f5cd1848181f5b5c797f8d5512ba177d6010ee92
salman644 <salmanquluzade20@gmail.com>
2024-12-26 00:11:09 +0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from app.routes.poll_routes import poll_routes
+from app.routes.vote_routes import vote_routes
+
+def create_app():
+    app = Flask(__name__)
+
+    # Register blueprints for routes
+    app.register_blueprint(poll_routes)
+    app.register_blueprint(vote_routes)
+
+    return app
 
